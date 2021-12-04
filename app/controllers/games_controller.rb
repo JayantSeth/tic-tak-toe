1af3df0f9534cf8c64c7e56160e35541d604e0cc
JayantSeth <jayant.seth94@outlook.com>
2021-12-04 23:19:15 +0530

    Created flow path for new game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,24 @@
-class GamesController < ApplicationController
+class GamesController < ApplicationController 
+  before_action :set_game, only: [:show]
+  def index 
+  end 
+
+  def new 
+    game = Game.new() 
+    if game.save 
+      session[:game] = game.id 
+      session[:new_game] = true 
+      redirect_to new_player_path 
+    else  
+      puts game.errors.full_messages 
+    end
+  end 
+
+  def show 
+  end
+
+  private 
+  def set_game 
+    @game = Game.find_by_id(params[:id]) 
+  end 
 end
--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,24 @@
 class PlayersController < ApplicationController
+  def new 
+    @player = Player.new() 
+  end
+
+  def create 
+    primary = session[:new_game] ? true : false 
+    game = Game.find_by_id(session[:game])
+    the_params = set_player 
+    the_params[:game] = game
+    player = Player.new(the_params)
+    if player.save 
+      session[:player] = player.id 
+      redirect_to controller: 'games', action: 'show', id: game.id 
+    else  
+      puts player.errors.full_messages 
+    end
+  end
+
+  private
+  def set_player
+    params.require(:player).permit(:name)
+  end
 end
